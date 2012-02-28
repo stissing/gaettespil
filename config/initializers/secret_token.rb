813535ac45cf887746d2c288e8dcc6f186676976
Martin Stig Stissing <martinstissing@gmail.com>
2012-02-28 20:59:59 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gaettespil::Application.config.secret_token = '1f050a65f39538ea245995302a111c1559cfe08f2dae0f57558d15e61542595e0d13bcd18f1d809fe1f07ff9c9272c94e0e84bfc0ad61a2379bed692e7651ac3'
