0d2505957ea1d395e93ab6257b0dbee2c04a5835
Martin Stig Stissing <martinstissing@gmail.com>
2012-06-11 22:50:05 +0200

    now with extra guesses for admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   include SessionsHelper
-
+  helper_method :admin?
+  
   before_filter :require_log_in
 
   def require_log_in
